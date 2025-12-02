b2369aa8f0e7e04709f6ab448340500c526f7f52
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2025-12-02 18:12:04 -0500

    feat: Complete day 2 part 1

--- 2025/rust/src/bin/day02.rs
+++ 2025/rust/src/bin/day02.rs
@@ -6,13 +6,23 @@ fn main() {
         .split(b',')
         .flat_map(|range| {
             let binding = String::from_utf8(range.unwrap()).unwrap();
-            let (a, b) = binding.split_once('-').unwrap();
+            let (a, b) = binding.trim().split_once('-').unwrap();
             a.parse::<i64>().unwrap()..=b.parse::<i64>().unwrap()
         })
         .map(|n| n.to_string())
         .filter(|n| n.len() % 2 == 0)
+        // .inspect(|n| println!("n: {n}"))
+        .filter(|n| {
+            let bytes = n.as_bytes();
+            for (p1, p2) in (0..(n.len() / 2)).zip((n.len() / 2)..n.len()) {
+                if bytes[p1] != bytes[p2] {
+                    return false;
+                }
+            }
+            true
+        })
         .inspect(|n| println!("n: {n}"))
-        .count();
+        .fold(0, |sum, n| sum + n.parse::<i64>().unwrap());
     // .split(",")
     // .flat_map(|range| {
     //     let (a, b) = range.split_once('-').expect("Invalid range given");
@@ -20,4 +30,5 @@ fn main() {
     //     let end: i64 = b.parse().unwrap();
     //     start..=end
     // });
+    println!("Output: {x}");
 }
